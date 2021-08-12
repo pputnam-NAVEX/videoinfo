65b6f28690f60d99dec343cd694bfe3f8bcf9dfb
pputnam-NAVEX <philip.putnam@navexglobal.com>
2021-08-11 18:09:24 -0700

    tutorial out of date, updating/upgrading to ensure working properly

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
+const path = require("path");
+const fs = require("fs");
 
-app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
-    mainWindow.loadURL(`file://${__dirname}/index.html`); 
-});
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+async function createWindow() {
+
+  // Create the browser window.
+  win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false, // is default value after Electron v5
+      contextIsolation: true, // protect against prototype pollution
+      enableRemoteModule: false, // turn off remote
+      preload: path.join(__dirname, "preload.js") // use a preload script
+    }
+  });
+
+  // Load app
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  // rest of code..
+}
+
+app.on("ready", createWindow);
 
 // receiving from index.html video file path variable
-ipcMain.on('')
+ipcMain.on("toMain", (event, args) => {
+    fs.readFile("path/to/file", (error, data) => {
+        ffmpeg.ffprobe(args, (err, metadata) => {
+            console.log('Video duration is: ' + metadata.format.duration);
+        });
+      
+    //   win.webContents.send("fromMain", responseObj);
+    });
+});
