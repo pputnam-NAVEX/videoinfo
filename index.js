09e5a5b1776b9ee071c9faa432340a9c170fe9ff
pputnam-NAVEX <philip.putnam@navexglobal.com>
2021-08-11 17:37:10 -0700

    setup IPC to communicate between HTML & Electron

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`); 
 });
+
+// receiving from index.html video file path variable
+ipcMain.on('')
